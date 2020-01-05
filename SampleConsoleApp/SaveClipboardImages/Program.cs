310af2871ca032b7623295bcfab7559d0059c1b5
nandhukumar86 <39767305+nandhukumar86@users.noreply.github.com>
2020-01-05 13:16:25 +0530

    intial check in for Save Clipboard images

---
+++ SampleConsoleApp/SaveClipboardImages/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace SaveClipboardImages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Title: ");
+            string title = Console.ReadLine();
+
+            Thread t = new Thread(() =>
+            {
+                SaveImagesFromClipboard(title);
+
+            });
+
+            t.SetApartmentState(ApartmentState.STA);
+            t.Start();
+
+            if(t.ThreadState != ThreadState.Running)
+            {
+                Console.WriteLine($"All your images are stored under D:/{title}");
+                Thread.Sleep(2000);
+            }
+            else
+            {
+                Console.WriteLine("Snapshot saver is still running!!");
+            }
+        }
+
+        private static void SaveImagesFromClipboard(string title)
+        {
+            while (true)
+            {
+                if (Clipboard.ContainsImage())
+                {
+                    var datetime = DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                    BitmapSource image = Clipboard.GetImage();
+                    using (var fileStream = new FileStream($"D:/{title}_{datetime}.png", FileMode.Create))
+                    {
+                        BitmapEncoder encoder = new PngBitmapEncoder();
+                        encoder.Frames.Add(BitmapFrame.Create(image));
+                        encoder.Save(fileStream);
+                    }
+                    Clipboard.Clear();
+                }
+            }
+        }
+    }
+}
